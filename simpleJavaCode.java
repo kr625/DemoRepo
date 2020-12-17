490b277dd4358ccfd24899d5e1a3ebed4cdb3675
kr625 <68609630+kr625@users.noreply.github.com>
2020-12-17 23:50:50 +0530

    Update simpleJavaCode.java

--- simpleJavaCode.java
+++ simpleJavaCode.java
@@ -1,6 +1,6 @@
 public class simpleJavaCode{
 public static void main(String args[]) {
 System.out.println("Hi, This is a training on basics of Jenkins");
-System.out.println("Added Line 2 in the github repo")
+System.out.println("Added Line 2 in the github repo");
 }
 }
